df67c6c296ceab65ee9a5f327ee4100335387392
felipepsantanna <56663217+felipepsantanna@users.noreply.github.com>
2020-03-09 06:12:29 -0300

    FINAL
    1. Todos os documentos do curso de asp.net core 1 e 2 da Alura

---
+++ Aulas/Aula1/CasaDoCodigo/Models/ViewModels/UpdateQuantidadeResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models.ViewModels
+{
+    public class UpdateQuantidadeResponse
+    {
+        public UpdateQuantidadeResponse(ItemPedido itemPedido, CarrinhoViewModel carrinhoViewModel)
+        {
+            ItemPedido = itemPedido;
+            this.carrinhoViewModel = carrinhoViewModel;
+        }
+
+        public ItemPedido ItemPedido { get; }
+        public CarrinhoViewModel carrinhoViewModel { get; }
+    }
+}
